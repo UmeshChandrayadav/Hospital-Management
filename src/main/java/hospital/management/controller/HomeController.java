f3cc068128216ea64d7286e5f53c7f9885575e89
umesh <umesh@gmail.com>
2026-06-23 18:18:44 +0530

    added homecontroller and swagger

---
+++ src/main/java/hospital/management/controller/HomeController.java
@@ -0,0 +1,18 @@
+package hospital.management.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("/")
+	public Map<String, String> home() {
+	    return Map.of(
+	        "status", "UP",
+	        "message", "Hospital Management System API is running",
+	        "author", "Umesh"
+	    );
+	}
+}
