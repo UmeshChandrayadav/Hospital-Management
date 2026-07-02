da5de15f5dbe0695ee57e15c56f8779ce66a5a80
Umesh <umeshpchandra12@gmail.com>
2026-07-02 19:57:49 +0530

    updated config

--- src/main/java/hospital/management/config/SwaggerConfig.java
+++ src/main/java/hospital/management/config/SwaggerConfig.java
@@ -1,23 +1,25 @@
 package hospital.management.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
 
 @Configuration
-@OpenAPIDefinition(
-	    info = @Info(
-	        title = "Hospital Management System API",
-	        version = "1.0",
-	        description = "REST API for managing Patients, Doctors, Departments, Appointments, Medical Records and Prescriptions.",
-	        contact = @Contact(
-	            name = "Umesh Chandra Yadav",
-	            email = "umeshpchandra12@gmail.com"
-	        )
-	    )
-	)
 public class SwaggerConfig {
+	 @Bean
+	    public OpenAPI hospitalAPI() {
 
+	        return new OpenAPI()
+	                .info(new Info()
+	                        .title("Hospital Management System API")
+	                        .version("1.0")
+	                        .description("REST API for managing Patients, Doctors, Departments, Appointments, Medical Records and Prescriptions.")
+	                        .contact(new Contact()
+	                                .name("Umesh Chandra Yadav")
+	                                .email("umeshpchandra12@gmail.com")));
+	    }
 }
